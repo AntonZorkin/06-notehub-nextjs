4e33a2fb07077d17323a190027fc2637da77ae87
Anton Zorkin <zanton1978@gmail.com>
2025-10-16 14:10:19 +0200

    commit message

---
+++ app/notes/page.tsx
@@ -0,0 +1,36 @@
+
+import {QueryClient,dehydrate, HydrationBoundary,} from '@tanstack/react-query';
+import { fetchNotes } from '@/lib/api';
+import NotesClient from './Notes.client';
+
+const INITIAL_PAGE = 1;
+const INITIAL_PER_PAGE = 12;
+const INITIAL_SEARCH = '';
+
+export default async function NotesPage() {
+  const queryClient = new QueryClient();
+  try {
+    await queryClient.prefetchQuery({
+      queryKey: ['notes', INITIAL_PAGE, INITIAL_SEARCH],
+      queryFn: () =>
+        fetchNotes({
+          page: INITIAL_PAGE,
+          perPage: INITIAL_PER_PAGE,
+          search: INITIAL_SEARCH,
+        }),
+    });
+  } catch (err: unknown) {
+      const message = err instanceof Error ? err.message : 'Unknown error';
+    throw new Error(`Could not fetch the list of notes. ${message}`);
+  }
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient
+        initialPage={INITIAL_PAGE}
+        initialPerPage={INITIAL_PER_PAGE}
+        initialSearch={INITIAL_SEARCH}
+      />
+    </HydrationBoundary>
+  );
+}
---
+++ types/note.ts
@@ -0,0 +1,12 @@
+export interface Note {
+  id: number;
+  title: string;
+  content: string;
+  tag: "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
+}
+
+export interface NewNote {
+  title: string;
+  content?: string;
+  tag: "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
+}
